rgzn/sf

commit 14903624da442fd2b8e4c8bef99e85f42e9668f1

fixes #701

R/sample.R
@@ -39,7 +39,7 @@ st_sample = function(x, size, ..., type = "random") {
 	if (length(size) > 1) {
 		size = rep(size, length.out = length(x))
 		st_set_crs(do.call(c, lapply(1:length(x),
-			function(i) st_sample(x[i], size[i], type = type))),
+			function(i) st_sample(x[i], size[i], type = type, ...))),
 			st_crs(x))
 	} else {
 		dim = max(st_dimension(x))
@@ -85,6 +85,10 @@ st_multipoints_sample = function(x, size, ..., type = "random") {
 }
 
 st_ll_sample = function(x, size, ..., type = "random") {
+	if (isTRUE(st_is_longlat(x))) {
+		message_longlat("st_sample")
+		st_crs(x) = NA_crs_
+	}
 	l = st_length(x)
 	d = runif(size, 0, sum(l))
 	lcs = c(0, cumsum(l))

tests/sfc.R
@@ -205,6 +205,8 @@ ls = st_sfc(st_linestring(rbind(c(0,0),c(0,1))),
  st_linestring(rbind(c(2,2),c(2,2.00001))))
 st_sample(ls, 80)
 st_sample(nc[1:2,], size = c(10,20))
+# try with LINES, LongLat, should generate a warning:
+nc[1:2,] %>% st_transform(4326) %>% st_cast("MULTILINESTRING") %>% st_sample(size = c(10,20))
 
 #class(st_bind_cols(nc, as.data.frame(nc)[1:3]))
 class(dplyr::bind_cols(nc, as.data.frame(nc)[1:3]))

tests/sfc.Rout.save
@@ -852,6 +852,18 @@ POINT (1271918 979879)
 POINT (1264558 935867)
 POINT (1256623 949384)
 POINT (1222665 972639)
+> # try with LINES, LongLat, should generate a warning:
+> nc[1:2,] %>% st_transform(4326) %>% st_cast("MULTILINESTRING") %>% st_sample(size = c(10,20))
+although coordinates are longitude/latitude, st_sample assumes that they are planar
+although coordinates are longitude/latitude, st_sample assumes that they are planar
+Geometry set for 2 features 
+geometry type:  MULTIPOINT
+dimension:      XY
+bbox:           xmin: -81.7023 ymin: 36.238 xmax: -80.9089 ymax: 36.578
+epsg (SRID):    4326
+proj4string:    +proj=longlat +datum=WGS84 +no_defs
+MULTIPOINT (-81.7023 36.4641, -81.6845 36.3652,...
+MULTIPOINT (-80.9639 36.469, -80.9089 36.5654, ...
 > 
 > #class(st_bind_cols(nc, as.data.frame(nc)[1:3]))
 > class(dplyr::bind_cols(nc, as.data.frame(nc)[1:3]))
@@ -885,10 +897,10 @@ POINT EMPTY
 Geometry set for 1 feature 
 geometry type:  POINT
 dimension:      XY
-bbox:           xmin: 0.0551135 ymin: 1.05473 xmax: 0.0551135 ymax: 1.05473
+bbox:           xmin: 0.040877 ymin: 1.09605 xmax: 0.040877 ymax: 1.09605
 epsg (SRID):    NA
 proj4string:    NA
-POINT (0.0551135 1.05473)
+POINT (0.040877 1.09605)
 > 
 > # st_bbox:
 > library(sp)
@@ -1041,4 +1053,4 @@ POINT (0 1)
 > 
 > proc.time()
    user  system elapsed 
-  2.650   0.304   2.662 
+  2.708   0.271   2.682 

commit ba6d033d4112e0c7f7d1d07502727f0d884cf9fd

st_ll_sample() changed to preserve input crs
Fixes a bug introduced in https://github.com/r-spatial/sf/commit/14903624da442fd2b8e4c8bef99e85f42e9668f1 that causes st_sample() to set the output crs to NA when input is a LINESTRING in longlat.

R/sample.R
@@ -141,6 +141,7 @@ st_multipoints_sample = function(x, size, ..., type = "random") {
 }
 
 st_ll_sample = function (x, size, ..., type = "random", offset = runif(1)) {
+	input_crs = st_crs(x)
 	if (isTRUE(st_is_longlat(x))) {
 		message_longlat("st_sample")
 		st_crs(x) = NA_crs_
@@ -163,7 +164,7 @@ st_ll_sample = function (x, size, ..., type = "random", offset = runif(1)) {
 		grp = split(d, cut(d, lcs, include.lowest = TRUE))
 		grp = lapply(seq_along(x), function(i) grp[[i]] - lcs[i])
 	}
-	st_sfc(CPL_gdal_linestring_sample(x, grp), crs = st_crs(x))
+	st_sfc(CPL_gdal_linestring_sample(x, grp), crs = input_crs)
 }
 
 ### hex grid that